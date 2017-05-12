dfb2202e45f9ac14035e320acbd523166ded9b57
Nick Forester <nforeste@ucsc.edu>
2017-05-12 16:36:02 -0700

    added basic building collision detection

--- js/prefabs/building.js
+++ js/prefabs/building.js
@@ -6,10 +6,8 @@
  * @param {number} h -- height of the building (in grid cells)
  * @param {string} key -- the cached key of the building sprite
  * @param {string} frame -- (optional) image frame in a texture atlas/spritesheet
- * @param {boolean} rotatable -- (optional) if the building can rotate (default: false)
- * @param {array} otherFrames -- (optional) list of frames for that building (default: false)
  */
-function Building(game, w, h, key, frame, rotatable, otherFrames) {
+function Building(game, w, h, key, frame) {
     Phaser.Sprite.call(this, game, 0, 0, key, frame);
 
     this.game = game;
@@ -17,16 +15,6 @@ function Building(game, w, h, key, frame, rotatable, otherFrames) {
     this.h = h;
     this.held = false;
     this.placed = false;
-    this.rotatable = rotatable || false;
-    this.orientation = {
-        x: 0,
-        y: 0
-    };
-    if (otherFrames) {
-        this.otherFrames = otherFrames;
-        this.otherFrames.push(frame);
-        this.frameIndex = 0;
-    }
 
     game.add.existing(this);
     game.gameWorld.add(this);
@@ -36,53 +24,59 @@ function Building(game, w, h, key, frame, rotatable, otherFrames) {
 
     this.inputEnabled = true;
     //give the sprite button functionality
-    this.events.onInputDown.add(this.clicked, this);
 
-    game.input.keyboard.addKey(Phaser.Keyboard.R).onDown.add(this.rotate, this);
+    //This will later be changed to somewhere else
+    //Because the building object should be created
+    //When this function is called
+    this.events.onInputDown.addOnce(this.purchased, this);
 }
 
 Building.prototype = Object.create(Phaser.Sprite.prototype);
 Building.prototype.constructor = Building;
 
-Building.prototype.clicked = function() {
-    if (this.placed) {
-        //open ui menu to look at building stats
-    } else if (this.held) {
-        //building no longer held, but placed
-        this.held = false;
-        this.placed = true;
+Building.prototype.purchased = function() {
+    this.alpha = .75;
+    this.held = true;
+    this.game.holdingBuilding = true;
+    this.game.gameWorld.bringToTop(this);
+    this.anchor.set(.5);
+    this.events.onInputDown.addOnce(Building.prototype.placed, this);
+};
 
-        //anchor back in upper left corner to line up with grid
+Building.prototype.placed = function() {
+    this.placed = true;
+    this.held = false;
+
+    //this.orientation only applies to walkways, otherwise it should set to 0
+    if (this.orientation) {
         this.anchor.x = this.orientation.x;
         this.anchor.y = this.orientation.y;
-        //put opacity back at full
-        this.alpha = 1;
-
-        this.game.holdingBuilding = false;
-
-        var xPos = this.game.g.xStart + this.game.g.upperLeftRow;
-        var yPos = this.game.g.yStart + this.game.g.upperLeftColumn;
-
-        //snap the building to the grid 
-        this.x = xPos * 32;
-        this.y = yPos * 32;
-
-        //marks the cell as occupied
-        this.game.g.cells[xPos][yPos] = 1;
-
-        //clear the grid highlights
-        this.game.g.bmdOverlay.clear();
     } else {
-        //building has been initially clicked, not yet placed
-        //reduce the opacity
-        this.alpha = .75;
-        this.held = true;
-        this.game.holdingBuilding = true;
-        this.game.gameWorld.bringToTop(this);
-
-        //set the anchor so that it is in the middle of the mouse
-        this.anchor.set(.5);
+        this.anchor.set(0);
     }
+
+    this.alpha = 1;
+    this.game.holdingBuilding = false;
+
+    var xPos = this.game.g.xStart + this.game.g.upperLeftRow;
+    var yPos = this.game.g.yStart + this.game.g.upperLeftColumn;
+
+    this.x = xPos * 32;
+    this.y = yPos * 32;
+
+    //update cell info here
+    for (let i = xPos; i < xPos + this.w; i++) {
+        for (let j = yPos; j < yPos + this.h; j++) {
+            this.game.g.cells[i][j].occupied = true;
+        }
+    }
+
+    this.game.g.bmdOverlay.clear();
+    this.events.onInputDown.add(Building.prototype.getInfo, this);
+};
+
+Building.prototype.getInfo = function() {
+    //UI popup to give player info about the building
 };
 
 Building.prototype.update = function() {
@@ -90,31 +84,118 @@ Building.prototype.update = function() {
         this.x = this.game.input.worldX / this.game.worldScale;
         this.y = this.game.input.worldY / this.game.worldScale;
 
-        this.game.g.draw(this.w, this.h, 0.25);
+        var xPos = this.game.g.xStart + this.game.g.upperLeftRow;
+        var yPos = this.game.g.yStart + this.game.g.upperLeftColumn;
+        var highlightColor = '#66ff33';
+        var opacity = .25;
+        this.events.onInputDown.active = true;
+
+        if (xPos && yPos) {
+            for (let i = xPos; i < xPos + this.w; i++) {
+                for (let j = yPos; j < yPos + this.h; j++) {
+                    if (this.game.g.cells[i][j].occupied) {
+                        highlightColor = '#facade';
+                        opacity = .5;
+                        this.events.onInputDown.active = false;
+                    }
+                }
+            }
+        }
+
+        this.game.g.draw(this.w, this.h, opacity, highlightColor);
     }
 };
 
-Building.prototype.rotate = function() {
-    if (this.held && this.rotatable) {
-        if (this.otherFrames !== undefined) {
-            //In this case, the building is 'rotated' by replacing its sprite
-            this.frameName = this.otherFrames[this.frameIndex++];
-            this.frameIndex %= this.otherFrames.length;
-            this.w = this.width / 32;
-            this.h = this.height / 32;
-        } else {
-            this.angle += 90;
-            //this ugly mess just moves the building's orientation so that it 
-            //can be anchored correctly after rotation
-            if (this.orientation.x === 0 && this.orientation.y === 0) {
-                this.orientation.y = 1;
-            } else if (this.orientation.x > 0 && this.orientation.y === 0) {
-                this.orientation.x = 0;
-            } else if (this.orientation.y > 0 && this.orientation.x === 0) {
-                this.orientation.x = 1;
-            } else {
-                this.orientation.y = 0;
-            }
-        }
+/**
+ * A sublclass of Building, this defines a building that can be rotated
+ */
+function RotatableBuilding(game, w, h, key, frame, otherFrames) {
+    Building.call(this, game, w, h, key, frame);
+
+    if (otherFrames) {
+        this.otherFrames = otherFrames;
+        this.otherFrames.push(frame);
+        this.frameIndex = 0;
+    }
+
+    var r = game.input.keyboard.addKey(Phaser.Keyboard.R);
+    r.onDown.add(this.rotate, this);
+}
+
+RotatableBuilding.prototype = Object.create(Building.prototype);
+RotatableBuilding.prototype.constructor = RotatableBuilding;
+
+RotatableBuilding.prototype.rotate = function() {
+    if (this.held && this.otherFrames) {
+        this.frameName = this.otherFrames[this.frameIndex++];
+        this.frameIndex %= this.otherFrames.length;
+        this.w = this.width / 32;
+        this.h = this.height / 32;
     }
 };
+
+/**
+ * A subclass of Building, the walkway is a special case
+ */
+function Walkway(game, w, h, key, frame) {
+    RotatableBuilding.call(this, game, w, h, key, frame);
+
+    this.orientation = {
+        x: 0,
+        y: 0
+    };
+    //0 = down, 1 = left, 2 = up, 3 = right
+    this.rotation = 0;
+}
+
+Walkway.prototype = Object.create(RotatableBuilding.prototype);
+Walkway.prototype.constructor = Walkway;
+
+Walkway.prototype.rotate = function() {
+    if (this.held) {
+
+        this.angle += 90;
+
+        if (this.orientation.x === 0 && this.orientation.y === 0) {
+            this.orientation.y = 1;
+        } else if (this.orientation.x > 0 && this.orientation.y === 0) {
+            this.orientation.x = 0;
+        } else if (this.orientation.y > 0 && this.orientation.x === 0) {
+            this.orientation.x = 1;
+        } else {
+            this.orientation.y = 0;
+        }
+
+    }
+};
+
+/**
+ * @override Building.prototype.changeType
+ */
+Walkway.prototype.changeType = function() {
+    var cells = this.game.g.cells;
+    var xPos = this.x / 32;
+    var yPos = this.y / 32;
+
+    if (cells[xPos - 1][yPos].connects.right && cells[xPos][yPos - 1].connects.down) {
+
+        this.frameName = 'WalkwayTShape';
+    }
+};
+
+function CommandCenter(game, w, h, key, frame) {
+    Building.call(this, game, w, h, key, frame);
+}
+
+CommandCenter.prototype = Object.create(Building.prototype);
+CommandCenter.prototype.constructor = CommandCenter;
+
+/**
+ * Habitation Unit inherits from the rotatable building object
+ */
+function HabUnit(game, w, h, key, frame, otherFrames) {
+    RotatableBuilding.call(this, game, w, h, key, frame, otherFrames);
+}
+
+HabUnit.prototype = Object.create(RotatableBuilding.prototype);
+HabUnit.prototype.constructor = HabUnit;
